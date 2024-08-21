a326a813d1f67fc9bf45de786eb0915ba1b6379f
Thandokazi <payithando@gmail.com>
2024-08-21 21:56:27 +0200

    Added a search Functionality in the JS  File

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
+function reloadWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+     let cityElement = document.querySelector("#city");
+
+   cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+  
+}
+
+function searchCity(city) {
+  let apiKey = "t0db2ac0a4ca00efa9ab2ao6364fb8f5";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(reloadWeather);
+}
+
 function searchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
-FormElement = document.querySelector("#search-form");
+let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", searchSubmit);
