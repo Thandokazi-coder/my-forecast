d06b5a56af57d452f4b0a30f3e453f0d6be77304
Thandokazi <payithando@gmail.com>
2024-08-21 15:01:41 +0200

    Renamed JS file  and Added id to the HTML file

--- script.js
+++
@@ -1,9 +0,0 @@
-function searchSubmit(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("search-form-input");
-  let cityElement = documnet.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
-}
-
-let searchForElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", searchSubmit);
